87f6c61f1235358a89585dc285786e1d1e58a2df
ramonfrombr <ramonfrombr@gmail.com>
2024-07-26 01:10:06 -0300

    Created container component that includes the teleprompt form to input prompt text

---
+++ app/components/container.tsx
@@ -0,0 +1,27 @@
+const Container = () => {
+  return (
+    <div className="flex flex-col items-center max-w-screen-lg m-auto h-screen p-10">
+      <h1 className="text-3xl py-10">CS50 Teleprompter</h1>
+      <p className="m-auto max-w-screen-sm text-center">
+        A teleprompter, also known as an autocue, is a device used in
+        television, film production, and public speaking to display a scrolling
+        script or speech for presenters to read from while appearing to maintain
+        eye contact with the audience or camera.
+      </p>
+
+      <textarea
+        className="w-full h-screen p-4 my-4 border border-gray-300 rounded-md shadow-md"
+        placeholder="Enter your text here"
+      ></textarea>
+
+      <a
+        href="/teleprompt"
+        className="px-4 py-2 text-white bg-blue-500 rounded-md"
+      >
+        Generate
+      </a>
+    </div>
+  );
+};
+
+export default Container;
